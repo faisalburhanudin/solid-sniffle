3a9c32d25f665a4cb860187e5ae2434ecab854ad
faisalburhanudin <faisalburhanudin@gmail.com>
2017-08-05 12:57:50 +0700

    user get

--- service/user.go
+++ service/user.go
@@ -17,14 +17,20 @@ type UserSaver interface {
 	Save(user *domain.User) *domain.User
 }
 
+type UserGetter interface {
+	Get(user domain.User) *domain.User
+}
+
 type UserService struct {
 	usernameChecker UsernameChecker
 	emailChecker    EmailChecker
 	userSaver       UserSaver
+	userGetter      UserGetter
 }
 
 var ErrorUsernameUsed = errors.New("Username sudah terpakai")
 var ErrorEmailUsed = errors.New("Email sudah terpakai")
+var ErrorUserNotFound = errors.New("User not found")
 
 // Register new user
 func (service UserService) Register(user *domain.User) (error) {
@@ -44,4 +50,13 @@ func (service UserService) Register(user *domain.User) (error) {
 	service.userSaver.Save(user)
 
 	return nil
+}
+
+// GetByUsername get user by username
+func (service UserService) Get(userFilter domain.User) (*domain.User, error) {
+	user := service.userGetter.Get(userFilter)
+	if user == nil {
+		return nil, ErrorUserNotFound
+	}
+	return user, nil
 }
--- service/user_test.go
+++ service/user_test.go
@@ -34,10 +34,41 @@ func (check mockEmailChecker) IsUsed(email string) bool {
 func TestUserService_RegisterEmailUser(t *testing.T) {
 	userService := UserService{
 		usernameChecker: mockUsernameChecker{isUsedReturn: false},
-		emailChecker: mockEmailChecker{isUsedReturn: true},
+		emailChecker:    mockEmailChecker{isUsedReturn: true},
 	}
 	err := userService.Register(&domain.User{})
 	if err != ErrorEmailUsed {
 		t.Errorf("got: %v, want: %v.", err, ErrorEmailUsed)
+	}
+}
+
+type mockUserGetter struct {
+	userReturn *domain.User
+}
+
+func (mock mockUserGetter) Get(user domain.User) *domain.User  {
+	return mock.userReturn
+}
+
+func TestUserService_Get_NotFound(t *testing.T) {
+	UserService := UserService{
+		userGetter:mockUserGetter{userReturn:nil},
+	}
+	_, err := UserService.Get(domain.User{Username:"faisal"})
+	if err != ErrorUserNotFound {
+		t.Errorf("got: %v, want: %v.", err, ErrorUserNotFound)
+	}
+}
+
+func TestUserService_Get(t *testing.T) {
+	want := domain.User{
+		Username: "faisal",
+	}
+	UserService := UserService{
+		userGetter:mockUserGetter{userReturn:&want},
+	}
+	got, _:= UserService.Get(domain.User{Username:"faisal"})
+	if got != &want {
+		t.Errorf("got: %v, want: %v.", got, want)
 	}
 }
